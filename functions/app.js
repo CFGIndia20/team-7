010e0ad3737ef71c7f6359863c3b32a62d61e3e1
Kenil Shah <shahkenil20@gmail.com>
2020-07-26 02:08:15 +0530

    add MongoDB connectivity

--- functions/app.js
+++ functions/app.js
@@ -2,20 +2,56 @@
 //const firebase = require('firebase-admin');
 const express = require('express');
 const app = express();
+const fs=require("fs");
+const bodyParser =	require("body-parser");
 const indexRoute = require("./routes/index");
+mongoose     =			require('mongoose'),
+  passport     =   		require("passport"),
+ LocalStrategy=         require("passport-local"),
+  methodOverride=        require("method-override")	,
+  User 		  =	        require("./models/user");
 //const engines=require("consolidate");
 /*const firebaseApp=firebase.initializeApp(
     functions.config().firebase
 );
 */
 
-app.use(express.static("../public"));
+//Requiring Routes
+app.use('/', indexRoute);
+
+
+var url="mongodb://localhost:27017/cfgteam-7";
+mongoose.connect(url,{useNewUrlParser:true});
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 //no need to use ejs extension
 app.set("view engine", "ejs");
 
-//calls routrer -index.js
-app.use('/', indexRoute);
+app.use(express.static("../public"));
+
+
+
+//Passport Configuration
+app.use(require("express-session")({
+	secret: "$Team7$",
+	resave:false,
+	saveUninitialized:false
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
+
 
 
 /*app.get('/',(req,res)=>{
--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,7 +1,10 @@
-
+const fs=require('fs');
 const express= require("express");
-const router = express.Router();
+const router = express.Router({mergeParams:true});
 //const request= require("request");
+var passport = require("passport");
+var User = require("../models/user");
+
 
 
 //route 1 //html ejs
@@ -9,15 +12,65 @@ router.get('/', (req,res)=>{
 	res.render("homepage");
 });
 
+
+
+
+router.get("/register",function(req,res){
+	res.render("register");
+});
+
+router.post("/register",function(req,res){
+
+	var newUser = new User({username:req.body.username});
+	User.register(newUser,req.body.password,function(err,user){
+		if(err){
+    	console.log(err);
+    	//return res.render("register", {error: err.message});
+}
+		else{
+			passport.authenticate("local")(req,res,function(){
+
+				res.redirect("/success");
+			});
+		}
+	});	
+
+
+})
+
+
+
 router.get("/login",(req,res)=>{
 	res.render("login");
 
 });
 
+//Handling Login logic
+//app.post("/login",middleware,callback)
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login'
+  }), (req, res) => {
+    if (req.user.isAdmin === true) {
+      res.redirect('/admin');
+    }
+    if (req.user.isAdmin === false) {
+      res.redirect('/donor');
+    }
+  });
+
+
 router.get("/admin",(req,res)=>{
 	res.render("admin");
 
 });
+
+
+router.get("/donor",function(req,res){
+	res.render("donor");
+})
+
 
 router.get("/rating",(req,res)=>{
 	res.render("rating");

192d7d641d1d0883a2fd3c622f387b1a08d58860
Kenil Shah <shahkenil20@gmail.com>
2020-07-25 17:34:51 +0530

    fix nodemon error

--- functions/app.js
+++ functions/app.js
@@ -15,7 +15,7 @@ app.use(express.static("../public"));
 app.set("view engine", "ejs");
 
 //calls routrer -index.js
-app.use('/', routerindexRoute);
+app.use('/', indexRoute);
 
 
 /*app.get('/',(req,res)=>{

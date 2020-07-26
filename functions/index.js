7b208a9ce12a7f641fee08f5f4f241ce55307c65
Anjali-Kan <anjali.kanvinde@gmail.com>
2020-07-26 10:14:18 +0530

    Collecting responses on database succesfully, added hindi questions, hosted functions on cloud

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,4 @@
-// const functions = require('firebase-functions');
-// const firebase = require('firebase-admin');
-// const sendMsg= require('./twilio');
-// const replyRecieve = require('./reply_to_msgs');
-// const firebaseApp=firebase.initializeApp(
-//     functions.config().firebase
-// );
-
-
-// exports.sendMsg=sendMsg.sendMessage;
-// console.log(replyRecieve);
-// exports.replyRecieve=functions.https.onRequest(replyRecieve);
-
-
-
+//import all needed dependencies
 require('dotenv').config();
 const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
@@ -21,18 +7,18 @@ const app = express();
 const http = require('http');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const twilio = require('twilio');
-
 const { ref } = require('firebase-functions/lib/providers/database');
 const { strict } = require('assert');
 firebase.initializeApp();
 const db = firebase.firestore();
-
-const qn=0;
+//end of dependencies
+//defaults setting 
+let qn=0;
 let responseTo='responseTo'
 let response ='';
-ans='Thank you for using St.Jude\'s facilities!\nFeedback form for unit 1:\nA. On a scale of 1 to 5 how clean was the unit? SMS A<YOUR_RATING>'
-function getMsg(qn){
-    switch(qn){
+ans='\n\nसेंटजुडे की सुविधाओं का उपयोग करने के लिए धन्यवाद!\nA) यूनिट 1 के लिए फीडबैक फॉर्म: 1 से 5 के पैमाने पर यूनिट कितनी साफ थी?\n\nC. 1 से 5 के पैमाने पर राशन की आपूर्ति कैसे होती थी?\n SMS C <your_RATING> SMS A <your_RATING>\n'
+function getMsg(qn){ //what question to ask should move it to db later.
+    switch(qn){ //change the questions later and give hindi
         case 0:
            return 'Thank you for using St.Jude\'s facilities!\nFeedback form for unit 1:\nA. On a scale of 1 to 5 how clean was the unit? SMS A<YOUR_RATING>'
             break;
@@ -50,67 +36,44 @@ function getMsg(qn){
             break;
     }
     
-}
+} // update qn_no and response collection in db
+async function updateData(newval){
+    await db.collection('users').doc("+919820054228").set(newval).then(()=>{
+        console.log("Answering succesfully!");
+    })
 
-async function readFlag()
+}
+async function readFlag(req) //check what  next ques to show for the database
 {
-    await db.collection('users').doc("+919820054228").get().then(doc=>{
-        functions.logger.log("+919820054228"+" doc:  "+ doc);
+    await db.collection('users').doc("+919820054228").get().then(doc=>{ //get user doc by phone
+        functions.logger.log("+919820054228"+" doc:  "+ doc.data());
         if(doc.exists){
             qn=doc.data.question_no;
             responseTo+=''+qn;
             response=req.body;
             ans=getMsg(qn);
-            db.collection('users').doc("+919820054228").set(newval).then(()=>{
-                console.log("Answering succesfully!");
-            })
+            newval={ //set to next ques no
+                'question_no':qn+1,
+                 responseTo:response //record user response
+                
+            }
+            updateData(newval); //update to new data
+           
         }
         else{
             newval={
-                'question_no':0,
-                 responseTo:response
+                'question_no':0 //if user doesnt exist default vals
                 
             }
-            db.collection('users').doc("+919820054228").set(newval).then(()=>{
-                console.log("Answering succesfully!");
-            })
+            updateData(newval);
             functions.logger.log("+919820054228"+"doesnt exist");
         }
     });
 }
-app.post('/sms', (req, res) => {
+app.post('/sms', (req, res) => { //main api hit for replies
     const twiml = new MessagingResponse();
-    readFlag();
-    // switch(qn){
-    //     case 0:
-    //         twiml.message('Thank you for using St.Jude\'s facilities!\nFeedback form for unit 1:\nA. On a scale of 1 to 5 how clean was the unit? SMS A<YOUR_RATING>');
-    //         break;
-    //     case 1:
-    //         twiml.message('B. On a scale of 1 to 5 how were the sleeping conditions? SMS B<YOUR_RATING>');
-    //         break;
-    //     case 2:
-    //         twiml.message('C. On a scale of 1 to 5 how were the ration supplies? SMS C<YOUR_RATING>');
-    //         break;
-    //     case 3:
-    //         twiml.message('D. On a scale of 1 to 5 how was the hospitability? SMS D<YOUR_RATING>');
-    //         break;
-    // }
-    // newval={
-    //     'question_no':qn+1,
-    //      responseTo:response
-        
-    // }
-    
-    // newval={
-    //     'question_no':qn+1,
-    //      responseTo:response
-        
-    // }
-    // db.collection('users').doc("+919820054228").set(newval).then(()=>{
-    //     console.log("Answering succesfully!");
-    // });
-
-    twiml.message(ans);
+    readFlag(req); //whats the question number
+    twiml.message(ans); //keep the text as sms body to send
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
   });
@@ -118,7 +81,7 @@ app.post('/sms', (req, res) => {
 //   http.createServer(app).listen(1337, () => {
 //     console.log('Express server listening on port 1337');
 //   });
-const accountSid = process.env.accountSid;
+const accountSid = process.env.accountSid; //Tying to keep everything private 
 const authToken = process.env.authToken;
 app.post('/send_msg', (req, res) => {
     const client = new twilio(accountSid, authToken);
@@ -165,10 +128,3 @@ exports.app = functions.https.onRequest(app);
 
 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
